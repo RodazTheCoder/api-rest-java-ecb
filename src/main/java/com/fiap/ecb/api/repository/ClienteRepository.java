5ee9a149a53b5ab9ada842a130b952f71d99c5ca
Rodrigo Cabrera <rodrigofhc41@gmail.com>
2025-02-27 09:04:59 -0300

    Feat: create ClienteRepository for data access

---
+++ src/main/java/com/fiap/ecb/api/repository/ClienteRepository.java
@@ -0,0 +1,7 @@
+package com.fiap.ecb.api.repository;
+
+import com.fiap.ecb.api.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+}
